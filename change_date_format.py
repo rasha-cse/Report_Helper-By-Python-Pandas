bfa23b6405b6243914f7500f106c7e98894bcfed
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2025-06-02 11:50:38 +0600

    Update and rename change_date_format_Therap.py to change_date_format.py

--- change_date_format.py
+++ change_date_format.py
@@ -1,28 +1,28 @@
 import pandas as pd
 
-input_unformatted_therap_path = r"C:\Users\rasha\PycharmProjects\SIS_Report_Helper\input\SIS_INFO_FXRAC.xlsx"
-output_formatted_therap_path = r"C:\Users\rasha\PycharmProjects\SIS_Report_Helper\output\Fomatted_Date_SIS_INFO_Therap_Prod.xlsx"
+input_unformatted_r_path = r"C:\Users\rasha\PycharmProjects\SIS_Report_Helper\input\SIS_INFO_FXRAC.xlsx"
+output_formatted_r_path = r"C:\Users\rasha\PycharmProjects\SIS_Report_Helper\output\Fomatted_Date_SIS_INFO_r_Prod.xlsx"
 input_unformatted_sis_online_path = r"C:\Users\rasha\PycharmProjects\SIS_Report_Helper\output\exists_only_at_sis_online.xlsx"
 output_formatted_sis_online_path = r"C:\Users\rasha\PycharmProjects\SIS_Report_Helper\output\Fomatted_Date_SIS_Online.xlsx"
 
-input_dataframe_therap = pd.read_excel(input_unformatted_therap_path, sheetname='Export Worksheet', na_values=['NA'])
+input_dataframe_r = pd.read_excel(input_unformatted_r_path, sheetname='Export Worksheet', na_values=['NA'])
 input_dataframe_sis_online = pd.read_excel(input_unformatted_sis_online_path, sheetname='Sheet1', na_values=['NA'])
-#print(input_dataframe_therap[:10])
+#print(input_dataframe_r[:10])
 
-################################# Change Date Format Therap ###############################################
+################################# Change Date Format r ###############################################
 
-input_dataframe_therap["CREATED"] = input_dataframe_therap["CREATED"].map(lambda x: str(x)[:9])
-input_dataframe_therap["UPDATED"] = input_dataframe_therap["UPDATED"].map(lambda x: str(x)[:9])
-input_dataframe_therap["STATUS_CHANGE_DATE"] = input_dataframe_therap["STATUS_CHANGE_DATE"].map(lambda x: str(x)[:9])
-input_dataframe_therap["COMPLETED_DATE"] = input_dataframe_therap["COMPLETED_DATE"].map(lambda x: str(x)[:9])
+input_dataframe_r["CREATED"] = input_dataframe_r["CREATED"].map(lambda x: str(x)[:9])
+input_dataframe_r["UPDATED"] = input_dataframe_r["UPDATED"].map(lambda x: str(x)[:9])
+input_dataframe_r["STATUS_CHANGE_DATE"] = input_dataframe_r["STATUS_CHANGE_DATE"].map(lambda x: str(x)[:9])
+input_dataframe_r["COMPLETED_DATE"] = input_dataframe_r["COMPLETED_DATE"].map(lambda x: str(x)[:9])
 
-# print(input_dataframe_therap[:10])
+# print(input_dataframe_r[:10])
 
-writer_sis_therap = pd.ExcelWriter(output_formatted_therap_path, engine='xlsxwriter')
+writer_sis_r = pd.ExcelWriter(output_formatted_r_path, engine='xlsxwriter')
 # Convert the dataframe to an XlsxWriter Excel object.
-input_dataframe_therap.to_excel(writer_sis_therap, index=False, sheet_name='Sheet1')
+input_dataframe_r.to_excel(writer_sis_r, index=False, sheet_name='Sheet1')
 # Close the Pandas Excel writer and output the Excel file.
-writer_sis_therap.save()
+writer_sis_r.save()
 
 
 ################################# Change Date Format SIS Online ###############################################
