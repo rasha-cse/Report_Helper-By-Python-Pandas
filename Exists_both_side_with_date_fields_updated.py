1e9f3f4ece610e591b92403d0110c253c499873e
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2025-06-02 11:50:06 +0600

    Update Exists_both_side_with_date_fields_updated.py

--- Exists_both_side_with_date_fields_updated.py
+++ Exists_both_side_with_date_fields_updated.py
@@ -2,18 +2,18 @@
 import pandas as pd
 import numpy as np
 
-#therap_input_file_with_date_fields = 'Therap_prod_with_all_date_fields_8_aug_2017_new(2).xlsx'
+#r_input_file_with_date_fields = 'r_prod_with_all_date_fields_8_aug_2017_new(2).xlsx'
 date = '8_sep_2017'
 
-input_therap_path = r"C:\Users\rasha\PycharmProjects\SIS_Report_Helper\output\selected_columns_therap_file_all_date_fields_" + date + ".xlsx"
+input_r_path = r"C:\Users\rasha\PycharmProjects\SIS_Report_Helper\output\selected_columns_r_file_all_date_fields_" + date + ".xlsx"
 input_sis_online_path = r"C:\Users\rasha\PycharmProjects\SIS_Report_Helper\output\selected_columns_sis_online_all_date_fields_" + date + ".xlsx"
 output_exists_both_sides_path = r"C:\Users\rasha\PycharmProjects\SIS_Report_Helper\output\exists_both_with_date_fields_" + date + ".xlsx"
 
 
-input_therap_dataframe = pd.read_excel(input_therap_path, sheetname='Sheet1', na_values=['NA'])
+input_r_dataframe = pd.read_excel(input_r_path, sheetname='Sheet1', na_values=['NA'])
 input_sis_online_dataframe = pd.read_excel(input_sis_online_path, sheetname='Sheet1', na_values=['NA'])
 
-exists_both_sides_dataframe = pd.merge(input_therap_dataframe, input_sis_online_dataframe, on=['SIS_ID'], how='left', indicator='Exist')
+exists_both_sides_dataframe = pd.merge(input_r_dataframe, input_sis_online_dataframe, on=['SIS_ID'], how='left', indicator='Exist')
 #df.drop('Rating', inplace=True, axis=1)
 exists_both_sides_dataframe['Exist'] = np.where(exists_both_sides_dataframe.Exist == 'both', True, False)
 #print (df[:10])
@@ -49,11 +49,11 @@ exists_both_sides["dateUpdated_SIS_Online"] = pd.to_datetime(exists_both_sides["
 exists_both_sides["dateUpdated_SIS_Online"] = exists_both_sides["dateUpdated_SIS_Online"].dt.strftime("%m/%d/%Y")   #("%d-%b-%y")
 #exists_both_sides["dateUpdated_SIS_Online"] = exists_both_sides["dateUpdated_SIS_Online"].map(lambda x: str(x)[:10])
 
-exists_both_sides.rename(columns={'SIS_STATUS': 'SIS_STATUS_THERAP', 'CREATED': 'CREATED_AT_THERAP', 'UPDATED': 'UPDATED_AT_THERAP', 'dateUpdated_SIS_Online': 'Date_Updated_SIS_Online'}, inplace=True)
+exists_both_sides.rename(columns={'SIS_STATUS': 'SIS_STATUS_r', 'CREATED': 'CREATED_AT_r', 'UPDATED': 'UPDATED_AT_r', 'dateUpdated_SIS_Online': 'Date_Updated_SIS_Online'}, inplace=True)
 
-exists_both_sides['Matched/Mismatched?'] = np.where(exists_both_sides['SIS_STATUS_THERAP'] == exists_both_sides['SIS_STATUS_SIS_Online'], 'Matched', 'Mismatched')  #Matched/Mismatched?
+exists_both_sides['Matched/Mismatched?'] = np.where(exists_both_sides['SIS_STATUS_r'] == exists_both_sides['SIS_STATUS_SIS_Online'], 'Matched', 'Mismatched')  #Matched/Mismatched?
 
-exists_both_sides = exists_both_sides[['SIS_ID', 'SIS_TRACKING_NUM', 'SIS_STATUS_THERAP', 'SIS_STATUS_SIS_Online', 'Matched/Mismatched?', 'CREATED_AT_THERAP', 'UPDATED_AT_THERAP', 'STATUS_CHANGE_DATE', 'COMPLETED_DATE', 'Date_Updated_SIS_Online']]
+exists_both_sides = exists_both_sides[['SIS_ID', 'SIS_TRACKING_NUM', 'SIS_STATUS_r', 'SIS_STATUS_SIS_Online', 'Matched/Mismatched?', 'CREATED_AT_r', 'UPDATED_AT_r', 'STATUS_CHANGE_DATE', 'COMPLETED_DATE', 'Date_Updated_SIS_Online']]
 
 writer_exists_both_sides = pd.ExcelWriter(output_exists_both_sides_path, engine='xlsxwriter')
 exists_both_sides.to_excel(writer_exists_both_sides, index=False, sheet_name='Sheet1')
